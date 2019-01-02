f0c51399b0ac4c039bda03259388474f49349847
YanTing1111 <1178170499@qq.com>
2019-01-02 21:09:26 +0800

    组件

---
+++ react/redux-shopping-cart/src/store/reducers/updateCartReducer.js
@@ -0,0 +1,12 @@
+import {} from "../actions/type";
+
+const initialState = {
+
+}
+
+export default function (state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
