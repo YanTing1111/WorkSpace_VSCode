0340dc287d1c308e960a4cf55a5eebf242275ee6
YanTing1111 <1178170499@qq.com>
2019-01-03 15:31:14 +0800

    浅拷贝的方法

---
+++ react/immutable/demo2.js
@@ -0,0 +1,12 @@
+// 深拷贝浅拷贝
+var arr = ["old", 1, true, null, undefined];
+var new_arr = arr.slice();//浅拷贝
+// var new_arr = arr.concat();//浅拷贝
+// var new_arr = arr 
+// 如果arr或new_arr一方修改，未来就不可知了，所以不要用
+// 用Immutable（不可变数据），外界无法修改它，
+// 基于它计算出来的一个新的状态不会修改老的状态
+// reducer 是一个纯函数 => 结果一定是确定的，不会出现模棱两可的情况
+new_arr[0] = "new";
+console.log(arr);
+console.log(new_arr);
