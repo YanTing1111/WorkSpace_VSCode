6381782d1ecf2eaeb7b2eda0245c5e02881b42b7
YanTing1111 <1178170499@qq.com>
2019-01-04 21:43:46 +0800

    实现注册

---
+++ react/redux-juejin/src/redux/store/store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { users } from "../reducer/reducer";
+
+
+const store = createStore(
+  combineReducers(users),
+  applyMiddleware(logger)
+);
+
+export default store;
