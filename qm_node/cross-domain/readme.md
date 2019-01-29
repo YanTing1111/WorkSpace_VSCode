3bd4a8ed1158ba2ad1b5eb86dc486a311ca1dc71
YanTing1111 <1178170499@qq.com>
2019-01-29 20:50:06 +0800

    '前后端获取cookie方法'

--- qm_node/cross-domain/readme.md
+++ qm_node/cross-domain/readme.md
@@ -1,3 +1,17 @@
 # 前端跨域解决方案
 
 - cookies
+  是一个网站为了识别用户身份，进行session跟踪，而存储在用户本地终端上的数据，
+  服务端和客户端都可操作和访问。
+  后端用ctx.cookies.set
+  前端用document.cookie 形式是以;隔开的key=val
+  1. path选项
+    指定cookie有效的路径
+  2. http-only
+    为true时，js无法获得cookie值，增强了cookie的安全性
+  3. maxAge
+    设置过期时间
+  4. expires
+    指定过期日期
+  5. overwrite
+    是否允许覆盖
--- qm_node/cross-domain/router/index.js
+++ qm_node/cross-domain/router/index.js
@@ -4,6 +4,19 @@ const home = new Router();
 
 home.get('/', async (ctx) => {
   const title = '首页';
+  ctx.cookies.set('username', 'lisa', {
+    domain: 'localhost',
+    path: '/',
+    maxAge: 1000*60*60*24*10,
+    expires: new Date('2019-10-1'),
+    httpOnly: false,
+    overwrite: false
+  })
+  ctx.cookies.set('uid', '1000', {
+    domain: 'localhost',
+    path: '/',
+    httpOnly: true
+  })
   ctx.body = title;
 });
 
