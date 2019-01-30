ea915b25db6e015e5949463404998a59dc394809
YanTing1111 <1178170499@qq.com>
2019-01-30 23:01:10 +0800

    'instagram的前端部分'

---
+++ react/instagram/client/webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const clientPath = path.resolve(__dirname);
+
+module.exports = {
+  
+}
